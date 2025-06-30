3d57296d2d750c38a50eea5f0718e88d88395ed7
siwanetzu <domgolab@gmail.com>
2025-06-30 23:23:59 +0100

    Added moon directional light and prepared variables for day night cycle (time, day length, start time, time rate and sun)

---
+++ scenes/day_night_cycle.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+var time: float
+@export var day_length: float = 20.0
+@export var start_time: float = 0.3
+var time_rate: float
+
+var sun : DirectionalLight3D
+
+
+
+func _ready() -> void:
+	pass
+	
