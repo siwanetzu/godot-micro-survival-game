62382982a0a6ac06f7fb2241f617793091127876
siwanetzu <domgolab@gmail.com>
2025-07-01 01:45:45 +0100

    setting needs ui bars + modular script for values and regen/decay rates

---
+++ scripts/need_bar.gd
@@ -0,0 +1,9 @@
+extends ProgressBar
+
+@export var need_name: String
+@onready var text: Label = get_node("NeedText")
+
+func update_value (new_value, max):
+	max_value = max
+	value = new_value
+	text.text = str(need_name, ": ", int(value), " / ", int(max_value))
