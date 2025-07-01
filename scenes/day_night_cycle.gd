7263843e69c25ac018b2f0dee7d2975d3671edf4
siwanetzu <domgolab@gmail.com>
2025-07-01 01:02:10 +0100

    Added full day/night cycle with animated sun/moon and dynamic sky/ambient lighting

--- scenes/day_night_cycle.gd
+++ scenes/day_night_cycle.gd
@@ -5,9 +5,26 @@ var time: float
 @export var start_time: float = 0.3
 var time_rate: float
 
+# sun
 var sun : DirectionalLight3D
 @export var sun_color: Gradient
 @export var sun_intensity: Curve
+
+# moon
+var moon : DirectionalLight3D
+@export var moon_color: Gradient
+@export var moon_intensity: Curve
+
+# ambient and sky
+@export var ambient_intensity: Curve
+@export var sky_energy: Curve
+
+var env: Environment
+var sky_material: ProceduralSkyMaterial
+
+# tinting sky dark blue
+@export var sky_top_color: Gradient
+@export var sky_horizon_color: Gradient
 
 
 func _ready() -> void:
@@ -15,6 +32,15 @@ func _ready() -> void:
 	time = start_time
 	
 	sun = get_node("Sun")
+	moon = get_node("Moon")
+	
+	var world_env = get_node("WorldEnvironment")
+	env = world_env.environment
+	
+	var sky_res = env.sky
+	if sky_res and sky_res.sky_material is ProceduralSkyMaterial:
+		sky_material = sky_res.sky_material
+	
 	
 func _process(delta: float) -> void:
 	time += time_rate * delta
@@ -22,6 +48,24 @@ func _process(delta: float) -> void:
 	if time > 1.0:
 		time = 0.0
 		
+
+	# sun
 	sun.rotation_degrees.x = time * 360 + 90
 	sun.light_color = sun_color.sample(time)
+	sun.light_energy = sun_intensity.sample(time)
 	
+	# moon 
+	moon.rotation_degrees.x = time * 360 + 270
+	moon.light_color = moon_color.sample(time)
+	moon.light_energy = moon_intensity.sample(time)
+	
+	# ambient energy
+	env.ambient_light_energy = ambient_intensity.sample(time)
+
+	# sky energy
+	if sky_material:
+		sky_material.energy_multiplier = sky_energy.sample(time)
+
+	# night sky
+	sky_material.sky_top_color = sky_top_color.sample(time)
+	sky_material.sky_horizon_color = sky_horizon_color.sample(time)
